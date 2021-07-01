ebc0c82156b69ab0e4a154a73bffca9e0f48bfc6
unknown <cmwillett87@gmail.com>
2021-07-01 10:53:40 -0400

    Created project and app and definned the models and migrated to db

---
+++ Flashcards_API/Flashcards/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Flashcard(models.Model):
+    collection = models.ForeignKey(Collection, blank=True, null=True, on_delete=models.CASCADE)
+    word = models.CharField(max_length=50)
+    definition = models.CharField(max_length=100)
+    card_number = models.IntegerField(default=0)
