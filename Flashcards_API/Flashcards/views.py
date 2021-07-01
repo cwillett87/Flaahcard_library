7b2d5b888c6fd2bc45a4467feb77470d04104394
unknown <cmwillett87@gmail.com>
2021-07-01 13:19:11 -0400

    created flashcard API views list, detail, flashcardNumber

--- Flashcards_API/Flashcards/views.py
+++ Flashcards_API/Flashcards/views.py
@@ -47,3 +47,68 @@ class CollectionDetail(APIView):
         collection = self.get_object(title)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class FlashcardList(APIView):
+
+    def get(self, request):
+        flashcard = Flashcard.objects.all()
+        serializer = FlashcardSerializer(flashcard, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FlashcardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReplyDetail(APIView):
+
+    def get_object(self, collection):
+        try:
+            return Flashcard.objects.filter(collection=collection)
+        except Collection.DoesNotExist:
+            raise Http404
+
+    def get(self, request, collection):
+        flashcard = self.get_object(collection)
+        serializer = FlashcardSerializer(flashcard, many=True)
+        return Response(serializer.data)
+
+    def put(self, request, collection):
+        flashcard = self.get_object(collection)
+        serializer = FlashcardSerializer(flashcard, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, collection):
+        flashcard = self.get_object(collection)
+        flashcard.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class FlashcardNumber(APIView):
+
+    def get_object(self, pk, word):
+        try:
+            return Flashcard.objects.get(pk=pk, word=word)
+        except Flashcard.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        flashcard = self.get_object(pk)
+        serializer = FlashcardSerializer(flashcard)
+        return Response(serializer.data)
+
+    def patch(self, request, pk, word):
+        flashcard = self.get_object(pk, word=word)
+        data = {"card_number": flashcard.card_number + int(1)}
+        serializer = FlashcardSerializer(flashcard, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
